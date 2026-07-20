fffaadaf2e76e58f0530c00261d2c46b8557ed47
Tannu Gupta <guptatannu538@gmail.com>
2026-07-20 12:46:00 +0530

    add experiment 1- LoR Baseline

---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,11 @@
+import dagshub
+import mlflow
+
+
+mlflow.set_tracking_uri('https://dagshub.com/guptatannu538/mlops-mini-project.mlflow')
+
+dagshub.init(repo_owner='guptatannu538', repo_name='mlops-mini-project', mlflow=True)
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
