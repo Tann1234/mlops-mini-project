b4af1dbdfc7efb330bacf0ba94e9c16eddb080cd
Tannu Gupta <guptatannu538@gmail.com>
2026-07-31 13:03:55 +0530

    correct verison 11

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,42 +1,25 @@
-# load test + signature test + performance test
-
 import unittest
 import mlflow
 import os
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle
-import dagshub
-
 
 class TestModelLoading(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-       import unittest
-import mlflow
-import os
-import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import pickle
-import time
-
-class TestModelLoading(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        # Check for DAGSHUB_PAT
+        # Set up DagsHub credentials for MLflow tracking
         dagshub_token = os.getenv("DAGSHUB_PAT")
         if not dagshub_token:
-            raise unittest.SkipTest("DAGSHUB_PAT not set, skipping model loading tests")
+            raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-        # Use username + token for authentication
-        os.environ["MLFLOW_TRACKING_USERNAME"] = "guptatannu538"  # replace with your DagsHub username
-        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
-
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token     
         mlflow.set_tracking_uri('https://dagshub.com/guptatannu538/mlops-mini-project.mlflow')
+
+
         # Load the new model from MLflow model registry
-        cls.new_model_name = "model"
+        cls.new_model_name = "my_model"
         cls.new_model_version = cls.get_latest_model_version(cls.new_model_name)
         cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
         cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
@@ -55,4 +38,5 @@ class TestModelLoading(unittest.TestCase):
 
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.new_model)
+
 
